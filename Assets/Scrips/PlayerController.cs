a5feb19760cce5f6c4f32e296b17aa5f7c7686ec
HappyPiggy <296056428@qq.com>
2016-12-18 00:24:32 +0800

    init ,create player

---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+    public float spinVelocity;
+    public float moveVelocity;
+
+
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+            return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        transform.Rotate(spinVelocity * Time.deltaTime * Vector3.up * h);
+        transform.Translate(moveVelocity * Time.deltaTime * Vector3.forward * v);
+    }
+}
